672d9675aa80d36329cabf182d08552ec5e65e46
ThiagoVP <thiagovp@hotmail.com.br>
2021-06-14 18:38:09 -0300

    CoAlUpdate de versão e adaptação do código.

--- src/main/java/com/veloso/learningspringboot/clientproxy/ClientProxyConfig.java
+++ src/main/java/com/veloso/learningspringboot/clientproxy/ClientProxyConfig.java
@@ -3,6 +3,7 @@ package com.veloso.learningspringboot.clientproxy;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,7 @@ public class ClientProxyConfig {
 
 	@Bean
 	public UserResourceV1 getUserResourceV1() {
-		ResteasyClient client = new ResteasyClientBuilder().build();
+		ResteasyClient client = new ResteasyClientBuilderImpl().build();
 		ResteasyWebTarget target = client.target(usersEndpointUrl);
 		UserResourceV1 proxy = target.proxy(UserResourceV1.class);
 		return proxy;
